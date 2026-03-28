1f2a80fb63b470e217f20114e2254555b308c8fc
zubimendim11-source <zubimendim11@gmail.com>
2026-03-28 11:50:14 +0300

    Create balances.py

---
+++ balances.py
@@ -0,0 +1,33 @@
+TEAM_BALANCES = {
+    # Top Clubs
+    "Arsenal": 150_000_000,
+    "Manchester City": 180_000_000,
+    "Manchester United": 140_000_000,
+    "Liverpool": 130_000_000,
+    "Chelsea": 160_000_000,
+    "Tottenham": 110_000_000,
+
+    # Strong Mid-table
+    "Aston Villa": 95_000_000,
+    "Newcastle": 120_000_000,
+    "Brighton": 75_000_000,
+    "West Ham": 80_000_000,
+    "Fulham": 65_000_000,
+
+    # Mid-table
+    "Brentford": 55_000_000,
+    "Everton": 50_000_000,
+    "Crystal Palace": 50_000_000,
+    "Bournemouth": 45_000_000,
+    "Wolves": 48_000_000,
+
+    # Bottom Table / Championship
+    "Forest": 40_000_000,
+    "Leeds": 40_000_000,
+    "Sunderland": 40_000_000,
+    "Burnley": 40_000_000
+}
+
+def get_balance(team_name):
+    """Возвращает баланс команды или 0, если команда не найдена"""
+    return TEAM_BALANCES.get(team_name, 0)
