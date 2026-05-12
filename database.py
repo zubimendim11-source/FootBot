2515ff5c327daef22de29f1bfc225880563cdc83
zubimendim11-source <zubimendim11@gmail.com>
2026-05-12 19:41:19 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
 import sqlite3
+import os
+
+# Получаем путь к папке, где лежит скрипт
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'game.db') # Убедись, что тут database.db
 
 def get_db():
-    conn = sqlite3.connect('football_bot.db', check_same_thread=False)
+    # Используем DB_PATH вместо простого текста
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False, timeout=30)
     conn.execute('PRAGMA busy_timeout = 5000') 
     conn.execute('PRAGMA journal_mode = WAL') 
     return conn
